bec9d65b3fed37916b258ab29d4cadfc9171380f
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-11 23:00:31 +0300

    feat/запуск миграций

---
+++ src/database/migrate.py
@@ -0,0 +1,17 @@
+import logging
+
+from yoyo import get_backend, read_migrations
+
+from src.settings import DB_SETTINGS
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+if __name__ == '__main__':
+    backend = get_backend(DB_SETTINGS.url)
+    migrations = read_migrations('/migrations')
+
+    with backend.lock():
+        backend.apply_migrations(backend.to_apply(migrations))
+    logger.info('Migrations were applied!')
