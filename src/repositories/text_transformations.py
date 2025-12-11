9d9d8dbab332763b98c8c09ee89d4ccd578ebcda
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-11 23:09:10 +0300

    feat/репозиторий преобразований текстов

---
+++ src/repositories/text_transformations.py
@@ -0,0 +1,29 @@
+from uuid import UUID
+
+from databases import Database
+
+
+class TextTransformationsRepository:
+    def __init__(self, *, db: Database | None = None) -> None:
+        super().__init__()
+        self.db = db
+
+    async def add_text_transformation(self, original_text: str, transformed_text: str, user_id: UUID | None):
+        query = """
+        INSERT INTO text_transformations(original_text, transformed_text, user_id)
+        VALUES (:original_text, :transformed_text, :user_id)
+        """
+        values = {
+            'original_text': original_text,
+            'transformed_text': transformed_text,
+            'user_id': user_id,
+        }
+        async with self.db.connection() as connection:
+            await connection.execute(query=query, values=values)
+
+    async def get_text_transformations(self, user_id: UUID | None):
+        query = 'SELECT * FROM text_transformations WHERE user_id = :user_id'
+        values = {'user_id': user_id}
+        async with self.db.connection() as connection:
+            raws = await connection.fetch_all(query=query, values=values)
+        return raws  # TODO: возвращать список dataclass'ов
