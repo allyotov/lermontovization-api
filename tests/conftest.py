7eb4effd913cc88fbc22237fe23792efaf3381cc
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-12 22:21:40 +0300

    feat/в conftest теперь только pytest_plugins (подключение фикстур)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,46 +1,7 @@
-from typing import Generator
-from unittest.mock import patch
-
-import pytest
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
-
-from src.main import app as original_app
-from src.services.lermontovization import LermontovizationService
-
-
-@pytest.fixture
-def anyio_backend():
-    return 'asyncio'
-
-
-@pytest.fixture
-def lermontovization_service():
-    return LermontovizationService()
-
-
-@pytest.fixture
-def mock_process_text():
-    with patch(
-        'src.services.lermontovization.LermontovizationService.process_text', return_value='МОК_Привет, мир!_МОК'
-    ):
-        yield
-
-
-@pytest.fixture
-def mock_process_text_demo():
-    with patch(
-        'src.services.lermontovization.LermontovizationService.process_text_demo', return_value='ДЕМО_Тест_ДЕМО'
-    ):
-        yield
-
-
-@pytest.fixture
-def app() -> FastAPI:
-    return original_app
-
-
-@pytest.fixture
-def client(app: FastAPI) -> Generator:
-    with TestClient(app=app) as client:
-        yield client
+pytest_plugins = [
+    'tests.fixtures.repositories.anyio_backend',
+    'tests.fixtures.repositories.db',
+    'tests.fixtures.repositories.text_transformations_repo',
+    'tests.fixtures.api_client.app',
+    'tests.fixtures.api_client.client',
+]
