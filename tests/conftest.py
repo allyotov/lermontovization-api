d828e6cc3efbd7d4d0f2375fd3600588ee61b24e
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-12 22:42:58 +0300

    feat/подключил фикстуры создания записи преобразования текста

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,6 @@ pytest_plugins = [
     'tests.fixtures.repositories.text_transformations_repo',
     'tests.fixtures.api_client.app',
     'tests.fixtures.api_client.client',
+    'tests.fixtures.make_test_transformation.make_text_db_record',
+    'tests.fixtures.make_test_transformation.make_text_transformation',
 ]
