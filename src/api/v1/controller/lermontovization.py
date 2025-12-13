42817472f61965ba71c41f19cfcdc514912387a9
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-13 20:52:19 +0300

    feat/обновил эндпойнты преобразования текста

--- src/api/v1/controller/lermontovization.py
+++ src/api/v1/controller/lermontovization.py
@@ -3,20 +3,18 @@ import logging
 from fastapi import APIRouter, Depends, Request
 from fastapi import status as http_status
 
+from src.deps import get_lermontovization_service
 from src.schemes.texts import InputText, OutputText
-from src.services.lermontovization import (
-    LermontovizationService,
-    get_lermontovization_service,
-)
+from src.services.lermontovization import LermontovizationService
 
-logger = logging.getLogger("TextLermontovizationController")
-lermontov_router = APIRouter(prefix="/text", tags=["text"])
+logger = logging.getLogger('TextLermontovizationController')
+lermontov_router = APIRouter(prefix='/text', tags=['text'])
 
 
 @lermontov_router.post(
-    "/lermontovizate",
-    summary="Заказать лермонтовизацию текста",
-    description="Заказать лермонтовизацию текста",
+    '/lermontovizate',
+    summary='Заказать лермонтовизацию текста',
+    description='Заказать лермонтовизацию текста',
     status_code=http_status.HTTP_201_CREATED,
 )
 async def lermontovizate_text(
@@ -24,14 +22,15 @@ async def lermontovizate_text(
     input_text: InputText,
     lermontov_service: LermontovizationService = Depends(get_lermontovization_service),
 ) -> OutputText:
-    processed_text = lermontov_service.process_text(text=input_text.text)
+    # TODO: добавить получение user_id из jwt токена (когда будет готова авторизация):
+    processed_text = await lermontov_service.process_text(text=input_text.text, user_id=None)
     return OutputText(text=processed_text)
 
 
 @lermontov_router.post(
-    "/demo-lermontovizate",
-    summary="Заказать лермонтовизацию текста",
-    description="Заказать лермонтовизацию текста",
+    '/demo-lermontovizate',
+    summary='Заказать лермонтовизацию текста',
+    description='Заказать лермонтовизацию текста',
     status_code=http_status.HTTP_201_CREATED,
 )
 async def demo_lermontovizate_text(
@@ -39,5 +38,5 @@ async def demo_lermontovizate_text(
     input_text: InputText,
     lermontov_service: LermontovizationService = Depends(get_lermontovization_service),
 ) -> OutputText:
-    processed_text = lermontov_service.process_text_demo(text=input_text.text)
+    processed_text = await lermontov_service.process_text(text=input_text.text, user_id=None)
     return OutputText(text=processed_text)
