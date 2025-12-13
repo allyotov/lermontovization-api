c54061b38c26c9ed1c5935e3b4d0844325d6d334
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-13 20:54:12 +0300

    feat/фикстуры добавления записи в таблицу texts

--- tests/fixtures/make_text_transformation.py
+++ tests/fixtures/make_text_transformation.py
@@ -1,27 +1,20 @@
-import datetime
 from uuid import UUID
 
 import pytest
 
-from src.models.text_transformation import TextTransformation
+from src.models.text_transformation import NewTextTransformation
 
 
 @pytest.fixture
-def make_text_transformation():
+def make_new_text_transformation():
     def inner(
-        id: UUID,
-        number: int,
         original_text: str,
         transformed_text: str,
-        created_at: datetime,
         user_id: UUID | None,
-    ) -> TextTransformation:
-        return TextTransformation(
-            id=id,
-            number=number,
+    ) -> NewTextTransformation:
+        return NewTextTransformation(
             original_text=original_text,
             transformed_text=transformed_text,
-            created_at=created_at,
             user_id=user_id,
         )
 
@@ -29,26 +22,23 @@ def make_text_transformation():
 
 
 @pytest.fixture
-def make_text_db_record(db, make_group_text_transformation):
+def make_text_db_record(db, make_new_text_transformation):
     async def inner(create=True, **kwargs):
-        text_transformation = make_group_text_transformation(**kwargs)
+        new_text_transformation = make_new_text_transformation(**kwargs)
         if not create:
-            return text_transformation
+            return new_text_transformation
 
         # TODO: записать компактнее, используя представление атрибутов dataclass'а как словаря:
         values = {
-            'id': text_transformation.id,
-            'number': text_transformation.number,
-            'original_text': text_transformation.original_text,
-            'transformed_text': text_transformation.transformed_text,
-            'created_at': text_transformation.create_at,
-            'user_id': text_transformation.user_id,
+            'original_text': new_text_transformation.original_text,
+            'transformed_text': new_text_transformation.transformed_text,
+            'user_id': new_text_transformation.user_id,
         }
 
         await db.execute(
             query="""
-            INSERT INTO text (id, number, original_text, transformed_text, created_at, user_id)
-            VALUES (:id, :number, :original_text, :transformed_text, :created_at, :user_id)
+            INSERT INTO texts (original_text, transformed_text, user_id)
+            VALUES (:original_text, :transformed_text, :user_id)
             """,
             values=values,
         )
