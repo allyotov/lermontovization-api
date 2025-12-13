c25c49f962c3bb11ba77d2175fdcf9bf1f244c30
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-13 18:37:30 +0300

    feat/исправил подключение фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,5 @@
 pytest_plugins = [
-    'tests.fixtures.repositories.anyio_backend',
-    'tests.fixtures.repositories.db',
-    'tests.fixtures.repositories.text_transformations_repo',
-    'tests.fixtures.api_client.app',
-    'tests.fixtures.api_client.client',
-    'tests.fixtures.make_test_transformation.make_text_db_record',
-    'tests.fixtures.make_test_transformation.make_text_transformation',
+    'tests.fixtures.repositories',
+    'tests.fixtures.api_client',
+    'tests.fixtures.make_test_transformation',
 ]
