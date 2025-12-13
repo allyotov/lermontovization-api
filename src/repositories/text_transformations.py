37287eb8f45bc2b2f70df72686fea2621377b9e1
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-13 20:53:09 +0300

    feat/доработал методы репозитория для записи и получения преобразований

--- src/repositories/text_transformations.py
+++ src/repositories/text_transformations.py
@@ -2,24 +2,33 @@ from uuid import UUID
 
 from databases import Database
 
-from src.models.text_transformation import TextTransformation
+from src.models.text_transformation import NewTextTransformation, TextTransformation
 
 
 class TextTransformationsRepository:
     def __init__(self, *, db: Database | None = None) -> None:
-        super().__init__()
         self.db = db
 
-    async def add_text_transformation(self, original_text: str, transformed_text: str, user_id: UUID | None):
-        query = """
-        INSERT INTO texts(original_text, transformed_text, user_id)
-        VALUES (:original_text, :transformed_text, :user_id)
-        """
-        values = {
-            'original_text': original_text,
-            'transformed_text': transformed_text,
-            'user_id': user_id,
-        }
+    async def add_text_transformation(self, new_text_transformation: NewTextTransformation) -> None:
+        if new_text_transformation.user_id is None:
+            query = """
+            INSERT INTO texts(original_text, transformed_text)
+            VALUES (:original_text, :transformed_text)
+            """
+            values = {
+                'original_text': new_text_transformation.original_text,
+                'transformed_text': new_text_transformation.transformed_text,
+            }
+        else:
+            query = """
+            INSERT INTO texts(original_text, transformed_text, user_id)
+            VALUES (:original_text, :transformed_text, :user_id)
+            """
+            values = {
+                'original_text': new_text_transformation.original_text,
+                'transformed_text': new_text_transformation.transformed_text,
+                'user_id': new_text_transformation.user_id,
+            }
         async with self.db.connection() as connection:
             await connection.execute(query=query, values=values)
 
@@ -30,11 +39,10 @@ class TextTransformationsRepository:
             rows = await connection.fetch_all(query=query, values=values)
         return [TextTransformation(**dict(row)) for row in rows]
 
-    async def get_guest_text_transformations(self, user_id: UUID | None):
+    async def get_guest_text_transformations(self):
         # TODO: добавить пагинацию (не через LIMIT OFFSET);
         # TODO: добавить сортировку по дате и номеру трансформации (возможно, в отдельном методе);
         query = 'SELECT * FROM texts'
-        values = {'user_id': user_id}
         async with self.db.connection() as connection:
-            rows = await connection.fetch_all(query=query, values=values)
+            rows = await connection.fetch_all(query=query)
         return [TextTransformation(**dict(row)) for row in rows]
