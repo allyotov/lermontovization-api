5ae0f693aa6eb884b4eb4b422dc40911a168219d
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-13 20:48:20 +0300

    feat/исправил фикстуры сервисов

--- tests/fixtures/services.py
+++ tests/fixtures/services.py
@@ -6,21 +6,13 @@ from src.services.lermontovization import LermontovizationService
 
 
 @pytest.fixture
-def lermontovization_service():
-    return LermontovizationService()
+def lermontovization_service(text_transformations_repo):
+    return LermontovizationService(text_transformations_repo)
 
 
 @pytest.fixture
 def mock_process_text():
     with patch(
         'src.services.lermontovization.LermontovizationService.process_text', return_value='МОК_Привет, мир!_МОК'
-    ):
-        yield
-
-
-@pytest.fixture
-def mock_process_text_demo():
-    with patch(
-        'src.services.lermontovization.LermontovizationService.process_text_demo', return_value='ДЕМО_Тест_ДЕМО'
-    ):
-        yield
+    ) as mock:
+        yield mock
